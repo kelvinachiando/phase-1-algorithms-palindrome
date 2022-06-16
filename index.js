6a58d72e3bba2a1435aed10194767fa624c28250
kelvinachiando <kelvinostin22gmail.com>
2022-06-16 21:58:54 +0300

    commit tree a6376e269e34a26e9a2b5bbbfa80350bc019db08

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord= word.split("").reverse().join("");
+  return reverseWord === word;
 }
 
 /* 
   Add your pseudocode here
+  //call the isPalindrome function
+  //determine whether the argument is palindrome or not
+  //return ture/false as per the end value
 */
 
 /*
   Add written explanation of your solution here
+  //The argument is taken in and used to make a new variable that is turned into an array of each character in the word, reversed in order, and joins them back together into a new string at the end.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+  console.log("")
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
